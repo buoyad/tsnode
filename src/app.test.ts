d4fa2e63f77e764ad8e0bc55bdfd3fc1fed3345c
Danny Ayoub <danny.a95@gmail.com>
2024-02-01 13:13:00 -0500

    add basic tests

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,1 +1,14 @@
 import app from './app'
+import request from 'supertest'
+
+describe('GET', () => {
+    it('should return 200 OK for /', async () => {
+        const server = request(app)
+        server.get('/').expect(200).end((err) => { if (err) throw err })
+    })
+
+    it('should return 200 OK and a specific response for /goodbye', async () => {
+        const server = request(app)
+        server.get('/goodbye').expect(200).expect('buh bye!').end((err) => { if (err) throw err })
+    })
+})
